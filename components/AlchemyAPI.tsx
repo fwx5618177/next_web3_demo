20c9d2831f13f2269cf2692bc731e93e941bd594
Wenxuan Feng <279357596@qq.com>
2022-12-01 21:36:50 +0800

    feat: alchemy-sdk

--- components/AlchemyAPI.tsx
+++ components/AlchemyAPI.tsx
@@ -1,60 +1,86 @@
-import {
-  Badge,
-  Heading,
-  Textarea,
-  Text,
-} from "@chakra-ui/react";
+import { Badge, Heading, Textarea, Text } from "@chakra-ui/react";
 import React from "react";
-import axios from 'axios'
-import conf from '../conf/alchemy.sdk.json'
+import axios from "axios";
+import { Network, Alchemy, AlchemySubscription } from "alchemy-sdk";
+import conf from "../conf/alchemy.sdk.json";
 import { hooks } from "../connectors/metaMask";
 
 export default function AlchemyAPI() {
   const [addresses, setAddresses] = React.useState<string[]>([]);
-  const [balances, setBalances] = React.useState<string>('')
+  const [balances, setBalances] = React.useState<string>("");
   const { useAccount } = hooks;
-  const account = useAccount()
+  const account = useAccount();
 
   const handleRequest = async (e: any) => {
     const data: string = (e?.target as HTMLInputElement)?.value;
-    const result: string[] = data?.split(",")?.map((ci) => ci?.trim())
-    const apiKey = conf.apiKey
-    const baseURL = `${conf.baseURL}${apiKey}`
+    const result: string[] = data?.split(",")?.map((ci) => ci?.trim());
+    const apiKey = conf.apiKey;
+    const baseURL = `${conf.baseURL}${apiKey}`;
+
+    if(!data) return 
 
     console.log(result);
-    
-    
+
     const reqData = JSON.stringify({
-      "jsonrpc": "2.0",
-      "method": "alchemy_getTokenBalances",
-      "params": [
-        `${account}`,
-        result,
-      ],
-      "id": 42
-    })
+      jsonrpc: "2.0",
+      method: "alchemy_getTokenBalances",
+      params: [`${account}`, result],
+      id: 42,
+    });
 
     const reqConfig = {
-      method: 'post',
+      method: "post",
       url: baseURL,
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      data : reqData
-    }
+      data: reqData,
+    };
 
-    const balances = await axios(reqConfig)
-    
-    const resultBalances = balances?.data?.result?.tokenBalances?.map((ci: { tokenBalance: any; }) => ci?.tokenBalance)
+    const balances = await axios(reqConfig);
 
-    console.log( balances?.data?.result?.tokenBalances?.map((ci: { tokenBalance: any; }) => ci?.tokenBalance), resultBalances);
-    
-    // const balances = await web3.alchemy.getTokenBalances(account as string, result)
+    const resultBalances = balances?.data?.result?.tokenBalances?.map(
+      (ci: { tokenBalance: any }) => ci?.tokenBalance
+    );
 
-    // setBalances(balances?.tokenBalances?.map(ci => ci?.tokenBalance).join(', '))
-    setBalances(resultBalances?.join(', '))
+    console.log(
+      balances?.data?.result?.tokenBalances?.map(
+        (ci: { tokenBalance: any }) => ci?.tokenBalance
+      ),
+      resultBalances
+    );
+
+    setBalances(resultBalances?.join(", "));
     setAddresses(result);
-  }
+  };
+
+  const handleRequestSDK = async (e: any) => {
+    const settings = {
+      apiKey: conf.apiKey,
+      network: Network.ETH_MAINNET,
+    };
+
+    const alchemy = new Alchemy(settings);
+
+    // Get the latest block
+    const latestBlock = alchemy.core.getBlockNumber();
+
+    // Get all outbound transfers for a provided address
+    alchemy.core
+      .getTokenBalances("0x994b342dd87fc825f66e51ffa3ef71ad818b6893")
+      .then(console.log);
+
+    // Get all the NFTs owned by an address
+    const nfts = alchemy.nft.getNftsForOwner("0xshah.eth");
+
+    // Listen to all new pending transactions
+    alchemy.ws.on(
+      { method: "alchemy_pendingTransactions" as AlchemySubscription, fromAddress: "0xshah.eth" },
+      (res) => console.log(res)
+    );
+
+    
+  };
 
   return (
     <>
@@ -66,7 +92,10 @@ export default function AlchemyAPI() {
       <Text mb={"8px"}>Address: {addresses.join(", ")}</Text>
       <Textarea
         placeholder="Here is a sample placeholder"
-        onKeyUp={handleRequest}
+        onKeyUp={
+          // handleRequest
+          handleRequestSDK
+        }
       />
       <Text mb={"8px"}>Balances: {balances}</Text>
     </>
